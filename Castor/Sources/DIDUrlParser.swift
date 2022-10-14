630648a88ce0ba38e89765e0b6c267b8109b8fc2
Goncalo-FradeIOHK <goncalo.frade@iohk.io>
2022-10-14 13:06:34 +0100

    feat(castor): add DIDUrl parser
    Fixes ATL-1995

---
+++ Castor/Sources/DIDUrlParser.swift
@@ -0,0 +1,85 @@
+import Antlr4
+import Domain
+import Foundation
+
+struct DIDUrlParser {
+    struct InvalidDIDStringError: Error {}
+
+    let didUrlString: String
+
+    func parse() throws -> DIDUrl {
+        let inputStream = ANTLRInputStream(didUrlString)
+        let lexer = DIDUrlAbnfLexer(inputStream)
+        let tokenStream = CommonTokenStream(lexer)
+
+        let parser = try DIDUrlAbnfParser(tokenStream)
+        parser.setErrorHandler(BailErrorStrategy())
+        let context = try parser.did_url()
+
+        let listener = Listener()
+        try ParseTreeWalker().walk(listener, context)
+
+        guard
+            let schema = listener.scheme,
+            let methodName = listener.methodName,
+            let methodId = listener.methodId
+        else { throw InvalidDIDStringError() }
+
+        let did = DID(
+            schema: schema,
+            method: methodName,
+            methodId: methodId
+        )
+        
+        let didUrl = DIDUrl(
+            did: did,
+            path: listener.path ?? [],
+            parameters: listener.query,
+            fragment: listener.fragment
+        )
+
+        return didUrl
+    }
+}
+
+fileprivate final class Listener: DIDUrlAbnfBaseListener {
+    fileprivate var scheme: String?
+    fileprivate var methodName: String?
+    fileprivate var methodId: String?
+    fileprivate var path: [String]?
+    fileprivate var query = [String: String]()
+    fileprivate var fragment: String?
+
+    override func exitDid(_ ctx: DIDUrlAbnfParser.DidContext) {
+        ctx.SCHEMA().map { scheme = $0.getText() }
+    }
+
+    override func exitMethod_name(_ ctx: DIDUrlAbnfParser.Method_nameContext) {
+        methodName = ctx.getText()
+    }
+
+    override func exitMethod_specific_id(
+        _ ctx: DIDUrlAbnfParser.Method_specific_idContext
+    ) {
+        methodId = ctx.getText()
+    }
+    
+    override func exitPath(_ ctx: DIDUrlAbnfParser.PathContext) {
+        guard !ctx.isEmpty() else { return }
+        path = ctx.string().map { $0.getText() }
+    }
+    
+    override func exitFrag(_ ctx: DIDUrlAbnfParser.FragContext) {
+        guard !ctx.isEmpty() else { return }
+        fragment = ctx.string()?.getText() ?? ctx.DIGIT()?.getText()
+    }
+    
+    override func exitSearchparameter(_ ctx: DIDUrlAbnfParser.SearchparameterContext) {
+        guard
+            !ctx.isEmpty(),
+            let key = ctx.string(0)?.getText(),
+            let value = ctx.string(1)?.getText()
+        else { return }
+        query[key] = value
+    }
+}
