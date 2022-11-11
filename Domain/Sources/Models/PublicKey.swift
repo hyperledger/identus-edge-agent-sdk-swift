fc269964f2afc11214432aec985a97221938f329
Goncalo-FradeIOHK <goncalo.frade@iohk.io>
2022-11-11 11:12:05 +0000

    feat(apollo): add keypair creation funcionality

---
+++ Domain/Sources/Models/PublicKey.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public struct PublicKey {
+    
+    public let curve: String
+    public let value: Data
+    
+    public init(curve: String, value: Data) {
+        self.curve = curve
+        self.value = value
+    }
+}
+
+public struct CompressedPublicKey {
+    
+    public let uncompressed: PublicKey
+    public let value: Data
+    
+    public init(uncompressed: PublicKey, value: Data) {
+        self.uncompressed = uncompressed
+        self.value = value
+    }
+}
