bfa5a06b62ad5a18ed598ed2b759ff9684e8d641
Goncalo-FradeIOHK <goncalo.frade@iohk.io>
2022-11-19 23:24:13 +0000

    feat(pluto): add core data model and entities
    Fixes ATL-2356
    
    Signed-off-by: goncalo-frade-iohk <goncalo.frade@iohk.io>

---
+++ Pluto/Sources/PersistentStorage/Models/CDDID+CoreDataProperties.swift
@@ -0,0 +1,17 @@
+import CoreData
+import Foundation
+
+public extension CDDID {
+    @nonobjc class func createFetchRequest() -> NSFetchRequest<CDDID> {
+        return NSFetchRequest<CDDID>(entityName: "CDDID")
+    }
+
+    @NSManaged var did: String
+    @NSManaged var schema: String
+    @NSManaged var method: String
+    @NSManaged var methodId: String
+}
+
+extension CDDID: Identifiable {
+    public var id: String { did }
+}
