f2c21422c2044de5b2bdd84a4a62b7c9cb0b224f
Goncalo-FradeIOHK <goncalo.frade@iohk.io>
2022-11-25 00:21:54 +0000

    feat(agent): enable parsing of enterprise invitation and add prism agent
    Fixes ATL-2497

---
+++ PrismAgent/Sources/Protocols/PrismOnboarding/PrismOnboardingInvitation.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+struct PrismOnboardingInvitation {
+    struct Body: Codable {
+        let type: String
+        let onboardEndpoint: String
+        let from: String
+    }
+
+    let body: Body
+
+    init(jsonString: String) throws {
+        guard let jsonData = jsonString.data(using: .utf8) else { throw PrismAgentError.invitationIsInvalidError }
+        let object = try JSONDecoder().decode(Body.self, from: jsonData)
+        guard object.type == ProtocolTypes.prismOnboarding.rawValue else {
+            throw PrismAgentError.unknownPrismOnboardingTypeError
+        }
+        self.body = object
+    }
+}
