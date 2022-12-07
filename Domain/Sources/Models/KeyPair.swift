66afe30455e87e660ed190a1ca5a5beab4498d1b
Goncalo-FradeIOHK <goncalo.frade@iohk.io>
2022-12-07 17:50:11 +0000

    feat(apollo): add x25519 and ed25519 cryptographic methods
    Fixes ATL-2657
    
    Signed-off-by: goncalo-frade-iohk <goncalo.frade@iohk.io>

--- Domain/Sources/Models/KeyPair.swift
+++ Domain/Sources/Models/KeyPair.swift
@@ -1,12 +1,29 @@
 import Foundation
 
+public enum KeyCurve: Equatable {
+    case x25519
+    case ed25519
+    case secp256k1(index: Int = 0)
+
+    public var name: String {
+        switch self {
+        case .x25519:
+            return "X25519"
+        case .ed25519:
+            return "Ed25519"
+        case .secp256k1:
+            return "secp256k1"
+        }
+    }
+}
+
 public struct KeyPair {
-    public let index: Int
+    public let curve: KeyCurve
     public let privateKey: PrivateKey
     public let publicKey: PublicKey
 
-    public init(index: Int = 0, privateKey: PrivateKey, publicKey: PublicKey) {
-        self.index = index
+    public init(curve: KeyCurve = .secp256k1(), privateKey: PrivateKey, publicKey: PublicKey) {
+        self.curve = curve
         self.privateKey = privateKey
         self.publicKey = publicKey
     }
