20544eee563f61193d91484cfcd740f11c4144b4
Goncalo-FradeIOHK <goncalo.frade@iohk.io>
2022-12-09 13:07:50 +0000

    feat(agent): add mediation key update and logic
    Fixes ATL-2701
    
    Signed-off-by: goncalo-frade-iohk <goncalo.frade@iohk.io>

--- Sample/AtalaPrismWalletDemo/AtalaPrismWalletDemo/Modules/SetupPrismAgent/SetupPrismAgentViewModel.swift
+++ Sample/AtalaPrismWalletDemo/AtalaPrismWalletDemo/Modules/SetupPrismAgent/SetupPrismAgentViewModel.swift
@@ -5,14 +5,12 @@ import Foundation
 final class SetupPrismAgentViewModelImpl: ObservableObject, SetupPrismAgentViewModel {
 
     @Published var status: String = ""
-    @Published var mediatorRoutingId: String = ""
+    @Published var error: String?
 
     private let agent: PrismAgent
 
     init() {
         let did = try! DID(string: "did:peer:2.Ez6LScc4S6tTSf5PnB7tWAna8Ee2aL7z2nRgo6aCHQwLds3m4.Vz6MktCyutFBcZcAWBnE2shqqUQDyRdnvcwqMTPqWsGHMnHyT.SeyJpZCI6Im5ldy1pZCIsInQiOiJkbSIsInMiOiJodHRwOi8vcm9vdHNpZC1tZWRpYXRvcjo4MDAwIiwiYSI6WyJkaWRjb21tL3YyIl19")
-
-        let oob = "http://rootsid-mediator:8000?_oob=eyJ0eXBlIjoiaHR0cHM6Ly9kaWRjb21tLm9yZy9vdXQtb2YtYmFuZC8yLjAvaW52aXRhdGlvbiIsImlkIjoiMmZjNzQyYjktZTFmOS00ODJlLTkxMmItOGY5MDhhODVhYTYxIiwiZnJvbSI6ImRpZDpwZWVyOjIuRXo2TFNjYzRTNnRUU2Y1UG5CN3RXQW5hOEVlMmFMN3oyblJnbzZhQ0hRd0xkczNtNC5WejZNa3RDeXV0RkJjWmNBV0JuRTJzaHFxVVFEeVJkbnZjd3FNVFBxV3NHSE1uSHlULlNleUpwWkNJNkltNWxkeTFwWkNJc0luUWlPaUprYlNJc0luTWlPaUpvZEhSd09pOHZjbTl2ZEhOcFpDMXRaV1JwWVhSdmNqbzRNREF3SWl3aVlTSTZXeUprYVdSamIyMXRMM1l5SWwxOSIsImJvZHkiOnsiZ29hbF9jb2RlIjoicmVxdWVzdC1tZWRpYXRlIiwiZ29hbCI6IlJlcXVlc3RNZWRpYXRlIiwibGFiZWwiOiJNZWRpYXRvciIsImFjY2VwdCI6WyJkaWRjb21tL3YyIl19fQ"
 
         self.agent = PrismAgent(mediatorServiceEnpoint: did)
         status = agent.state.rawValue
@@ -22,10 +20,25 @@ final class SetupPrismAgentViewModelImpl: ObservableObject, SetupPrismAgentViewM
         await MainActor.run {
             status = agent.state.rawValue
         }
-        try await agent.start()
+        do {
+            try await agent.start()
+        } catch {
+            await MainActor.run {
+                self.error = error.localizedDescription
+            }
+        }
         await MainActor.run {
             status = agent.state.rawValue
-            mediatorRoutingId = agent.connectionManager.mediator?.routingDID.string ?? ""
+        }
+    }
+
+    func updateKeyList() async throws {
+        do {
+            _ = try await agent.createNewPeerDID(updateMediator: true)
+        } catch {
+            await MainActor.run {
+                self.error = error.localizedDescription
+            }
         }
     }
 }
