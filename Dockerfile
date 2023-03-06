1c77ca6f1c9ccc9c1e440ee47544b2c1bf6189b7
goncalo-frade-iohk <goncalo-frade-iohk@users.noreply.github.com>
2023-03-06 11:49:32 +0000

    feat(docs): (879e47d) Merge d93273f400c9262bf2b30618edeafb093b8f054d into 07efc8b3f178e3702f4808a852224e57d149522b

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM nginx:1.23.3
-COPY . /usr/share/nginx/html
-RUN rm /etc/nginx/conf.d/default.conf
-COPY ./nginx/nginx.conf /etc/nginx/conf.d
-
-EXPOSE 80
--- build_docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "${BASH_SOURCE[0]}")" || exit 126
-VERSION=0.0.1
-docker build -t ghcr.io/input-output-hk/prism-swift-docs:${VERSION} .
-docker push ghcr.io/input-output-hk/prism-swift-docs:${VERSION}
