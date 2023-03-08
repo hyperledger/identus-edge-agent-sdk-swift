76ddbac0f77f60e11fd9842a826346a4096bf081
Goncalo-FradeIOHK <goncalo.frade@iohk.io>
2023-03-08 11:56:38 +0000

    feat(apollo): update package.swift remove 1.4 and code related and just use secp256k1.swift lib
    secp256k1 lib uses libsecp256k1 internally that is the same one used by Prism SDK1.4 internally
    
    Signed-off-by: goncalo-frade-iohk <goncalo.frade@iohk.io>

--- AtalaPrismSDK/Apollo/Sources/Extensions/PrivateKey+Helpers.swift
+++
@@ -1,9 +0,0 @@
-import Domain
-import Foundation
-import PrismAPI
-
-extension Domain.PrivateKey {
-    init(curve: KeyCurve, fromEC: ECPrivateKey) {
-        self.init(curve: curve, value: fromEC.getEncoded().toData())
-    }
-}
