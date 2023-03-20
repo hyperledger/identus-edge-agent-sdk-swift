2a82cafbd5c4f48a7b9b2109fd4efe56b2c87a6d
Goncalo-FradeIOHK <goncalo.frade@iohk.io>
2023-03-20 10:27:52 +0000

    fix(castor): need to update antlr library and files to build

--- AtalaPrismSDK/Castor/Sources/DIDParser/DIDGrammar/DIDAbnfBaseListener.swift
+++ AtalaPrismSDK/Castor/Sources/DIDParser/DIDGrammar/DIDAbnfBaseListener.swift
@@ -1,6 +1,7 @@
-// Generated from java-escape by ANTLR 4.11.1
+// Generated from DIDAbnf.g4 by ANTLR 4.12.0
 
 import Antlr4
+
 
 /**
  * This class provides an empty implementation of {@link DIDAbnfListener},
@@ -8,81 +9,81 @@ import Antlr4
  * of the available methods.
  */
 open class DIDAbnfBaseListener: DIDAbnfListener {
-    public init() {}
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    open func enterDid(_ ctx: DIDAbnfParser.DidContext) {}
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    open func exitDid(_ ctx: DIDAbnfParser.DidContext) {}
+     public init() { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	open func enterDid(_ ctx: DIDAbnfParser.DidContext) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	open func exitDid(_ ctx: DIDAbnfParser.DidContext) { }
 
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    open func enterMethod_name(_ ctx: DIDAbnfParser.Method_nameContext) {}
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    open func exitMethod_name(_ ctx: DIDAbnfParser.Method_nameContext) {}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	open func enterMethod_name(_ ctx: DIDAbnfParser.Method_nameContext) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	open func exitMethod_name(_ ctx: DIDAbnfParser.Method_nameContext) { }
 
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    open func enterMethod_specific_id(_ ctx: DIDAbnfParser.Method_specific_idContext) {}
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    open func exitMethod_specific_id(_ ctx: DIDAbnfParser.Method_specific_idContext) {}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	open func enterMethod_specific_id(_ ctx: DIDAbnfParser.Method_specific_idContext) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	open func exitMethod_specific_id(_ ctx: DIDAbnfParser.Method_specific_idContext) { }
 
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    open func enterIdchar(_ ctx: DIDAbnfParser.IdcharContext) {}
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    open func exitIdchar(_ ctx: DIDAbnfParser.IdcharContext) {}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	open func enterIdchar(_ ctx: DIDAbnfParser.IdcharContext) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	open func exitIdchar(_ ctx: DIDAbnfParser.IdcharContext) { }
 
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    open func enterEveryRule(_ ctx: ParserRuleContext) throws {}
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    open func exitEveryRule(_ ctx: ParserRuleContext) throws {}
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    open func visitTerminal(_ node: TerminalNode) {}
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    open func visitErrorNode(_ node: ErrorNode) {}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	open func enterEveryRule(_ ctx: ParserRuleContext) throws { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	open func exitEveryRule(_ ctx: ParserRuleContext) throws { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	open func visitTerminal(_ node: TerminalNode) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	open func visitErrorNode(_ node: ErrorNode) { }
 }
