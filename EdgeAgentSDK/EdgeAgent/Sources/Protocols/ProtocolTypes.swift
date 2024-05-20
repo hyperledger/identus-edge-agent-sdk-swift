dde2b1af298b5abe941361e4dd1aff6fc32b1657
goncalo-frade-iohk <goncalo.frade@iohk.io>
2024-05-20 13:41:24 +0100

    feat(agent): revocation notification

--- EdgeAgentSDK/EdgeAgent/Sources/Protocols/ProtocolTypes.swift
+++ EdgeAgentSDK/EdgeAgent/Sources/Protocols/ProtocolTypes.swift
@@ -22,6 +22,7 @@ public enum ProtocolTypes: String {
     case didcommRequestCredential3_0 = "https://didcomm.org/issue-credential/3.0/request-credential"
     case didcommconnectionRequest = "https://atalaprism.io/mercury/connections/1.0/request"
     case didcommconnectionResponse = "https://atalaprism.io/mercury/connections/1.0/response"
+    case didcommRevocationNotification = "https://atalaprism.io/revocation_notification/1.0/revoke"
     case didcomminvitation = "https://didcomm.org/out-of-band/2.0/invitation"
     case prismOnboarding = "https://atalaprism.io/did-request"
     case pickupRequest = "https://didcomm.org/messagepickup/3.0/delivery-request"
